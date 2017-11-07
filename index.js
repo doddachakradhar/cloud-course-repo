358394876b124d95d30f14951066358c8211bf50
doddachakradhar <dodda.chakradhar2014@vit.ac.in>
2017-11-07 20:18:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 	const server = http.createServer((req, res) => {
   	res.statusCode = 200;
   		res.setHeader('Content-Type', 'text/plain');
-  		res.end('Hello World\n');
+  		res.end('Hi Sir,Im D.CHAKRADHAR (14BC0465) this is my first app\n');
 	});
 
 	server.listen(port, hostname, () => {
